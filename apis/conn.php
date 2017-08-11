fab01c29e4be19ae1c7826026a8cda6dfbb4b6d6
Patricio99 <patonaveira@gmail.com>
2017-08-11 15:25:45 -0300

    agrego apis

---
+++ apis/conn.php
@@ -0,0 +1,7 @@
+<?php
+$HostName = "localhost";
+$HostUser = "id2246846_root";
+$HostPass = "12345";
+$DatabaseName = "id2246846_dbproyectofinal";
+$conn = new mysqli($HostName, $HostUser, $HostPass, $DatabaseName);
+?>
